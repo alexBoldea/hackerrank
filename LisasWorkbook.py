64dc15c3fa0b31afe3ed2a48b8ae234cdec9b758
alexBoldea <38548951+alexBoldea@users.noreply.github.com>
2021-03-09 08:15:57 +0200

    Update LisasWorkbook.py
    now with actual running code and all tests passed

--- LisasWorkbook.py
+++ LisasWorkbook.py
@@ -2,32 +2,33 @@ def workbook(n, k, arr):
     """n is the number of chapters
     k is the number of problems per page
     arr is the list of the number of problems in each chapter
-
     n = 5 (also length of arr)
     k = 3
     arr = [4, 2, 6, 1, 10]
-
     if a problem number in a chapter is the same as a page number -> add 1"""
-
-    pages_in_chapter = {}
-    for i in range(1, n+1):
-        pages_in_chapter[i] = arr[i-1]//k + 1 if arr[i-1]%k else arr[i-1]//k # {1: 2, 2: 1, 3: 2, 4: 1, 5: 4}
-    page_numbers = {}
-    current_page = 1
-    for i in range(1, n+1):
-        page_numbers[i] = []
-        for j in range(pages_in_chapter[i]):
-            page_numbers[i].append(current_page)
-            current_page += 1
-    # page_numbers = {1: [1, 2], 2: [3], 3: [4, 5], 4: [6], 5: [7, 8, 9, 10]}
+    page_problems = {}
     magic = 0
+    pages_counted = 1
     for i in arr:
-        for j in range(1, i+1):
-            countdown = k
-            while k:
-                if j in page_numbers[i]:
-                    magic += 1
-                k -= 1
-
-
-workbook(5, 3, [4, 2, 6, 1, 10])
+        countdown_from = i        
+        problem_no = 1
+        while countdown_from:
+            page_problems[pages_counted] = []
+            if countdown_from//k > 0:
+                for _ in range(k):
+                    page_problems[pages_counted].append(problem_no)
+                    problem_no += 1
+                countdown_from -= k
+                pages_counted += 1
+            elif countdown_from//k == 0:
+                for _ in range(countdown_from%k):
+                    page_problems[pages_counted].append(problem_no)
+                    problem_no += 1
+                pages_counted += 1
+                countdown_from = 0
+            else:
+                break
+    for i in page_problems.keys():
+        if i in page_problems[i]:
+            magic += 1
+    return magic
